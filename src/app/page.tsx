7e452c4a199e464586737c0d76a5b1b179728821
20061920 <20061920@student.westernsydney.edu.au>
2026-08-03 23:57:38 +1000

    added login component into main page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import { db } from "~/server/db";
-
+import { LoginForm } from "./components/loginForm";
 export const dynamic = "force-dynamic";
 
 export default async function Home() {
@@ -12,8 +12,9 @@ export default async function Home() {
     <main className="min-h-screen bg-zinc-50 px-6 py-16 dark:bg-black">
       <div className="mx-auto max-w-2xl">
         <h1 className="text-3xl font-semibold text-zinc-900 dark:text-zinc-50">
-          Smart Civic
+          Login
         </h1>
+        <LoginForm />
         <p className="mt-2 text-zinc-600 dark:text-zinc-400">
           Local Government Service Request Portal
         </p>
